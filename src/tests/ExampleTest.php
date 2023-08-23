bf9973e4bcc561feb976a0da8d056bdb371228fa
MasatoshiYamaguchi <masatoshi.yamaguchi@asia-quest.jp>
2023-08-23 17:39:13 +0900

    テスト用のクラス追加

---
+++ src/tests/ExampleTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace tests;
+
+use App\Example;
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    /**  @test */
+    public function test_example()
+    {
+        $example = new Example();
+        $this->assertSame('test', $example->test());
+    }
+}
